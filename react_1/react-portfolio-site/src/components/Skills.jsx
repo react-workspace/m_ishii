e52b4995b9d9774a4096310528b688eb70dd8a8d
m0216i <m0216i@outlook.com>
2022-06-01 19:00:45 +0900

    #2 ポートフォリオ作成　4-2~4-6

---
+++ react_1/react-portfolio-site/src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skills = () => {
+	return (
+		<div className="id">
+			<div className="conainer">
+				<div className="heading">
+					<h2>Skills</h2>
+				</div>
+				<div className="skills-container"></div>
+			</div>
+		</div>
+	)
+}
+
+export default Skills
