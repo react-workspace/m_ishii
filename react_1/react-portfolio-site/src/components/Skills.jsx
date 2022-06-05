99d5435a94ba7136db2fa50c7317e4bd4de5c977
m0216i <m0216i@outlook.com>
2022-06-05 21:03:49 +0900

    #5 ステートに応じて動的にViewを更新する　

--- react_1/react-portfolio-site/src/components/Skills.jsx
+++ react_1/react-portfolio-site/src/components/Skills.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { skillReducer, initialState, actionTypes } from 'react';
-import { useEffect, useReducer } from 'react';
 import axios from 'axios'
+import Circle from 'react-circle';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { useEffect, useReducer } from 'react';
+import { requestStates } from '../constants';
 
 export const Skills = () => {
 
@@ -9,7 +10,7 @@ export const Skills = () => {
 
 	useEffect(() => {
 		dispatch({ type: actionTypes.fetch });
-		axios.get('https://api.github.com/users/USER_NAME/repos')
+		axios.get('https://api.github.com/users/m0216i/repos')
 			.then((response) => {
 				const languageList = response.data.map(res => res.language);
 				const countedLanguageList = generateLanguageCountObj(languageList);
@@ -32,13 +33,41 @@ export const Skills = () => {
 		});
 	};
 
+	const converseCountToPercentage = (count) => {
+		if (count > 10) { return 100; }
+		return count * 10;
+	};
+
+	const sortedLanguageList = () => (
+		state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count)
+	)
+
 	return (
 		<div className="id">
 			<div className="conainer">
 				<div className="heading">
 					<h2>Skills</h2>
 				</div>
-				<div className="skills-container"></div>
+				<div className="skills-container">
+					 {
+            state.requestState === requestStates.loading && (
+              <p className="description">取得中...</p>
+            )
+					}
+					{
+						state.requestState === requestStates.success && (
+							sortedLanguageList().map((item, index) => (
+								<div className="skill-item" key={index}>
+									<p className="description"><strong>{item.language}</strong></p>
+									<Circle
+										animate
+										progress={converseCountToPercentage(item.count)}
+									/>
+								</div>
+							))
+						)
+					}
+				</div>
 			</div>
 		</div>
 	)
